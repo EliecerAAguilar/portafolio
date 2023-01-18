aa286c0a75adf23a5bf795cb88420926e498cad6
Eliecer Aguilar <eliecer.aguilar1@utp.ac.pa>
2023-01-17 23:10:09 -0500

    Updates

---
+++ precache-manifest.303713df0bb809d30c080bfbed49522b.js
@@ -0,0 +1,254 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49d183addda53bba14347bf5be74538d",
+    "url": "/portafolio/index.html"
+  },
+  {
+    "revision": "865ca5eba8a0f602ec3b",
+    "url": "/portafolio/static/css/main.5d51fb70.chunk.css"
+  },
+  {
+    "revision": "12f6a63c230ca8aa0cc9",
+    "url": "/portafolio/static/js/2.bf8aa514.chunk.js"
+  },
+  {
+    "revision": "865ca5eba8a0f602ec3b",
+    "url": "/portafolio/static/js/main.8872d236.chunk.js"
+  },
+  {
+    "revision": "4ed1e78690587181760b",
+    "url": "/portafolio/static/js/runtime-main.ca322516.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portafolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "e56d1862b363f35a363ca99ddb1dd297",
+    "url": "/portafolio/static/media/DesarrolloFrontEndServerlessBadge.e56d1862.png"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/portafolio/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/portafolio/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/portafolio/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/portafolio/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/portafolio/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/portafolio/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "ca26c83c0f651fa8dda6d37d61df6348",
+    "url": "/portafolio/static/media/Groovy_para_procesamiento_en_lote.ca26c83c.png"
+  },
+  {
+    "revision": "d5aa38367a30dd935a34f8a558c4582a",
+    "url": "/portafolio/static/media/Introducción_a_Docker.d5aa3836.png"
+  },
+  {
+    "revision": "41a948c51e6b11e56377333273295e75",
+    "url": "/portafolio/static/media/Introducción_a_Kafka.41a948c5.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portafolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "0c248021493345f3acbf7a5fbe8d107e",
+    "url": "/portafolio/static/media/Practicas_agiles_con_Jira.0c248021.png"
+  },
+  {
+    "revision": "9751b44f39b6de01fa45659b5ca9e8f5",
+    "url": "/portafolio/static/media/Principios_de_base_de_datos_MySQL.9751b44f.png"
+  },
+  {
+    "revision": "5194cd26c86adbeba1d4c64d766f9284",
+    "url": "/portafolio/static/media/Pruebas_automatizadas.5194cd26.png"
+  },
+  {
+    "revision": "16559172ca1d4d552390b48f19e102ba",
+    "url": "/portafolio/static/media/address_image.16559172.svg"
+  },
+  {
+    "revision": "dcd6dee2bd7458a4a576dc131506fd83",
+    "url": "/portafolio/static/media/animated_logo.dcd6dee2.svg"
+  },
+  {
+    "revision": "058eca4cbdc03a1dad9a6bb943ac7831",
+    "url": "/portafolio/static/media/blogs_image.058eca4c.svg"
+  },
+  {
+    "revision": "87eeeb3855fd0dd8aa106b2704b2f5a7",
+    "url": "/portafolio/static/media/ccna-introduction-to-networks.87eeeb38.png"
+  },
+  {
+    "revision": "cacbc42b661f39344af81c631d8d36a9",
+    "url": "/portafolio/static/media/cloud_infrastructure.cacbc42b.svg"
+  },
+  {
+    "revision": "fee92c18b1cfbd1faa704c51047f113d",
+    "url": "/portafolio/static/media/codeInLogo.fee92c18.png"
+  },
+  {
+    "revision": "5f915405511f649dad433a6db9adbff4",
+    "url": "/portafolio/static/media/contactMail.5f915405.png"
+  },
+  {
+    "revision": "4fe455a7edfc12344f890629e0938183",
+    "url": "/portafolio/static/media/control_de_versiones_badge.4fe455a7.png"
+  },
+  {
+    "revision": "ebf35d7f33ebbd4a4c6cff7e66e21fdb",
+    "url": "/portafolio/static/media/data_science.ebf35d7f.svg"
+  },
+  {
+    "revision": "3e23ca14e7a7c8a7b63d255466749000",
+    "url": "/portafolio/static/media/developerActivity.3e23ca14.svg"
+  },
+  {
+    "revision": "3bab7a9dc4324f1b2364d25a47fcfcf0",
+    "url": "/portafolio/static/media/education.3bab7a9d.svg"
+  },
+  {
+    "revision": "a00ede3f70759480e81f14f203edaa51",
+    "url": "/portafolio/static/media/experience.a00ede3f.svg"
+  },
+  {
+    "revision": "0017cc6de3f72f67753efa37bb796fbd",
+    "url": "/portafolio/static/media/fa-brands-400.0017cc6d.svg"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/portafolio/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/portafolio/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/portafolio/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/portafolio/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/portafolio/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/portafolio/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/portafolio/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "e647ddbb6d95cb01deac07ffafb2a085",
+    "url": "/portafolio/static/media/fa-regular-400.e647ddbb.svg"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/portafolio/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/portafolio/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/portafolio/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "424c59fb1df79fcc3d0a41909c0192d2",
+    "url": "/portafolio/static/media/fa-solid-900.424c59fb.svg"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/portafolio/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/portafolio/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "2936ce95f550e3feef7230ea543a4b60",
+    "url": "/portafolio/static/media/feelingProud.2936ce95.svg"
+  },
+  {
+    "revision": "fce6080726685cabdb4f75a0d8018325",
+    "url": "/portafolio/static/media/fullstack.fce60807.svg"
+  },
+  {
+    "revision": "6eeb2e810d0fd9f3ca2dcd72de228e68",
+    "url": "/portafolio/static/media/github_logo.6eeb2e81.png"
+  },
+  {
+    "revision": "0c8a4c114ad00f7854e91550379b9b91",
+    "url": "/portafolio/static/media/googleAssistant.0c8a4c11.jpg"
+  },
+  {
+    "revision": "a6addc3fb6605480614b992ede8cd434",
+    "url": "/portafolio/static/media/googleAssistant.a6addc3f.svg"
+  },
+  {
+    "revision": "715072c1bd745c9ad2acc8bc0ab9c785",
+    "url": "/portafolio/static/media/ipt_rio_hato_logo2.715072c1.png"
+  },
+  {
+    "revision": "5b4a6449747ec61a6d9aa874f2a9ceba",
+    "url": "/portafolio/static/media/jsFramework.5b4a6449.svg"
+  },
+  {
+    "revision": "c597fbded5de799dfb9abc3fc4f1c26f",
+    "url": "/portafolio/static/media/legato_logo.c597fbde.png"
+  },
+  {
+    "revision": "ced98694866977290c99e415afa6c3c6",
+    "url": "/portafolio/static/media/logo_utp.ced98694.png"
+  },
+  {
+    "revision": "83ac24fb968a474c6a63f7a4e9a8f62c",
+    "url": "/portafolio/static/media/manOnTable.83ac24fb.svg"
+  },
+  {
+    "revision": "140facd16b821b2b7481b937285e90b5",
+    "url": "/portafolio/static/media/profile_img_eliecer.140facd1.png"
+  },
+  {
+    "revision": "5faf790badc18fa9030f94836ece1df1",
+    "url": "/portafolio/static/media/programmer.5faf790b.svg"
+  },
+  {
+    "revision": "b8ba948796d7ab532673c5ed2f315e74",
+    "url": "/portafolio/static/media/projects_image.b8ba9487.svg"
+  },
+  {
+    "revision": "6ac91b161c2c9934a44f72d75d18142d",
+    "url": "/portafolio/static/media/talksCardBack.6ac91b16.svg"
+  },
+  {
+    "revision": "3fa5424232370e3d049c9555d9c440a4",
+    "url": "/portafolio/static/media/talksCardBackAlt.3fa54242.svg"
+  },
+  {
+    "revision": "d63630893eb8cb64af65f395d393c481",
+    "url": "/portafolio/static/media/ui_ux_design.d6363089.svg"
+  }
+]);
